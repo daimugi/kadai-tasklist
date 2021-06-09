e37708d7791630e24671245bb45764b822d04cfb
dai.mugi <dxinchuan@gmail.com>
2021-06-09 12:51:14 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  get 'toppages/index'
+  root to: 'toppages#index'
   
-  resources :tasks
+  get 'signup', to: 'user#new'
+  resources :tasks, only: [:index, :show, :create]
 end
