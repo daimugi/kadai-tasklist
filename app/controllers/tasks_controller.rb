cd566f0e6a84d9635e08e3c9f1655c5446973190
dai.mugi <dxinchuan@gmail.com>
2021-06-14 13:14:45 +0000

    edit3 task app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in 
+  #before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    if logged_in?
-      @task = current_user.tasks.build
+      #@task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc)
-    end
-     # @tasks = Task.all
   end
 
   def show
